bf7667e13b9243b19a9b11d03fa262d23c3aac0e
yang <jackyang9112@gmail.com>
2022-09-27 14:57:54 +0800

    Update http_client.py

--- examples/bert/pipeline/http_client.py
+++ examples/bert/pipeline/http_client.py
@@ -5,7 +5,7 @@ import requests
 
 url = "http://127.0.0.1:18082/code-review/prediction"
 
-data = {"key": ["text"],
+data = {"key": ["0"],
         "value": ["国有企业引入非国有资本对创新绩效的影响——基于制造业国有上市公司的经验证据"]}
 
 r = requests.post(url=url, data=json.dumps(data))
